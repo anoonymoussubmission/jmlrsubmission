cc63f62f92b27058a6b5aab123bbefe4938354b1
anoonymoussubmission <72822238+anoonymoussubmission@users.noreply.github.com>
2021-10-04 16:12:12 +0200

    Update data_select_utils.py

--- preprocessing_utils/data_select_utils.py
+++ preprocessing_utils/data_select_utils.py
@@ -18,31 +18,31 @@ def select_dataset(dataset_name, input_dim=2, n_samples=10000):
     :params n_samples: number of points returned. If 0, all datapoints will be returned. For artificial data, it will throw an error.
     """
     if dataset_name == 'fmnist':
-        f_mnist = FashionMNIST(root="./datasets", download=True)
+        f_mnist = FashionMNIST(root="./downloads", download=True)
         data = f_mnist.data.numpy()
         vec_data = np.reshape(data, (data.shape[0], -1))
         vec_data = np.float32(vec_data)
         labels = f_mnist.targets.numpy()
     elif dataset_name == 'emnist':
-        f_mnist = EMNIST(root="./datasets", download=True, split='byclass')
+        f_mnist = EMNIST(root="./downloads", download=True, split='byclass')
         data = f_mnist.data.numpy()
         vec_data = np.reshape(data, (data.shape[0], -1))
         vec_data = np.float32(vec_data)
         labels = f_mnist.targets.numpy()
     elif dataset_name == 'kmnist':
-        f_mnist = KMNIST(root="./datasets", download=True)
+        f_mnist = KMNIST(root="./downloads", download=True)
         data = f_mnist.data.numpy()
         vec_data = np.reshape(data, (data.shape[0], -1))
         vec_data = np.float32(vec_data)
         labels = f_mnist.targets.numpy()
     elif dataset_name == 'usps':
-        f_mnist = USPS(root="./datasets", download=True)
+        f_mnist = USPS(root="./downloads", download=True)
         data = f_mnist.data
         vec_data = np.reshape(data, (data.shape[0], -1))
         vec_data = np.float32(vec_data)
         labels = np.float32(f_mnist.targets)
     elif dataset_name == 'news':
-        newsgroups_train = fetch_20newsgroups(data_home='./datasets', subset='train',
+        newsgroups_train = fetch_20newsgroups(data_home='./downloads', subset='train',
                                               remove=('headers', 'footers', 'quotes'))
         vectorizer = TfidfVectorizer()
         vec_data = vectorizer.fit_transform(newsgroups_train.data).toarray()
@@ -56,8 +56,7 @@ def select_dataset(dataset_name, input_dim=2, n_samples=10000):
         labels = np.float32(train_data[:, -1])
     elif dataset_name == 'char':
         digits = datasets.load_digits()
-        n_samples = len(digits.images)
-        data = digits.images.reshape((n_samples, -1))
+        data = digits.images.reshape((len(digits.images), -1))
         vec_data = np.float32(data)
         labels = digits.target
 
@@ -67,63 +66,63 @@ def select_dataset(dataset_name, input_dim=2, n_samples=10000):
         vec_data, labels = data[0], data[1]
 
     elif dataset_name == 'kdd_cup':
-        cover_train = fetch_kddcup99(data_home='./datasets', download_if_missing=True)
+        cover_train = fetch_kddcup99(data_home='./downloads', download_if_missing=True)
         vec_data = cover_train.data
         string_labels = cover_train.target
         vec_data, labels = feature_tranformers.vectorizer_kdd(data=vec_data, labels=string_labels)
     elif dataset_name == 'aggregation':
         file_name = file_path + "/2d_data/Aggregation.csv"
-        a = np.array(pd.read_csv(file_name, sep=';'))
+        a = np.array(pd.read_csv(file_name, sep=';', header=None))
         vec_data = a[:, 0:2]
         labels = a[:, 2]
     elif dataset_name == 'compound':
         file_name = file_path + "/2d_data/Compound.txt"
-        a = np.array(pd.read_csv(file_name, sep='\t'))
+        a = np.array(pd.read_csv(file_name, sep='\t', header=None))
         vec_data = a[:, 0:2]
         labels = a[:, 2]
     elif dataset_name == 'd31':
         file_name = file_path + "/2d_data/D31.txt"
-        a = np.array(pd.read_csv(file_name, sep='\t'))
+        a = np.array(pd.read_csv(file_name, sep='\t', header=None))
         vec_data = a[:, 0:2]
         labels = a[:, 2]
     elif dataset_name == 'flame':
         file_name = file_path + "/2d_data/flame.txt"
-        a = np.array(pd.read_csv(file_name, sep='\t'))
+        a = np.array(pd.read_csv(file_name, sep='\t', header=None))
         vec_data = a[:, 0:2]
         labels = a[:, 2]
     elif dataset_name == 'path_based':
         file_name = file_path + "/2d_data/pathbased.txt"
-        a = np.array(pd.read_csv(file_name, sep='\t'))
+        a = np.array(pd.read_csv(file_name, sep='\t', header=None))
         vec_data = a[:, 0:2]
         labels = a[:, 2]
     elif dataset_name == 'r15':
         file_name = file_path + "/2d_data/R15.txt"
-        a = np.array(pd.read_csv(file_name, sep='\t'))
+        a = np.array(pd.read_csv(file_name, sep='\t', header=None))
         vec_data = a[:, 0:2]
         labels = a[:, 2]
     elif dataset_name == 'spiral':
         file_name = file_path + "/2d_data/spiral.txt"
-        a = np.array(pd.read_csv(file_name, sep='\t'))
+        a = np.array(pd.read_csv(file_name, sep='\t', header=None))
         vec_data = a[:, 0:2]
         labels = a[:, 2]
     elif dataset_name == 'birch1':
         file_name = file_path + "/2d_data/birch1.txt"
-        a = np.array(pd.read_csv(file_name, delimiter=r"\s+"))
+        a = np.array(pd.read_csv(file_name, delimiter=r"\s+", header=None))
         vec_data = a[:, :]
         labels = np.ones((vec_data.shape[0]))
     elif dataset_name == 'birch2':
         file_name = file_path + "/2d_data/birch2.txt"
-        a = np.array(pd.read_csv(file_name, delimiter=r"\s+"))
+        a = np.array(pd.read_csv(file_name, delimiter=r"\s+", header=None))
         vec_data = a[:, :]
         labels = np.ones((vec_data.shape[0]))
     elif dataset_name == 'birch3':
         file_name = file_path + "/2d_data/birch3.txt"
-        a = np.array(pd.read_csv(file_name, delimiter=r"\s+"))
+        a = np.array(pd.read_csv(file_name, delimiter=r"\s+", header=None))
         vec_data = a[:, :]
         labels = np.ones((vec_data.shape[0]))
     elif dataset_name == 'worms':
         file_name = file_path + "/2d_data/worms/worms_2d.txt"
-        a = np.array(pd.read_csv(file_name, sep=' '))
+        a = np.array(pd.read_csv(file_name, sep=' ', header=None))
         vec_data = a[:, :]
         labels = np.ones((vec_data.shape[0]))
     elif dataset_name == 't48k':
@@ -157,7 +156,7 @@ def select_dataset(dataset_name, input_dim=2, n_samples=10000):
         vec_data = np.random.uniform(0, 1, size=(n_samples, input_dim)) * 10
         labels = np.ones(n_samples)
     elif dataset_name == 'mnist_pc':
-        d_mnist = MNIST(root="./datasets", download=True)
+        d_mnist = MNIST(root="./downloads", download=True)
         mnist = d_mnist.data.numpy()
         data = np.float32(np.reshape(mnist, (mnist.shape[0], -1)))
         pca_data = PCA(n_components=input_dim).fit_transform(data)
@@ -165,7 +164,7 @@ def select_dataset(dataset_name, input_dim=2, n_samples=10000):
         vec_data = pca_data[n_indices]
         labels = d_mnist.targets.numpy()[n_indices]
     elif dataset_name == 'usps_pc':
-        d_mnist = USPS(root="./datasets", download=True)
+        d_mnist = USPS(root="./downloads", download=True)
         mnist = d_mnist.data
         data = np.float32(np.reshape(mnist, (mnist.shape[0], -1)))
         pca_data = PCA(n_components=input_dim).fit_transform(data)
@@ -183,17 +182,26 @@ def select_dataset(dataset_name, input_dim=2, n_samples=10000):
         vec_data = pca_data[n_indices]
         labels = targets
     else:
-        d_mnist = MNIST(root="./datasets", download=True)
+        d_mnist = MNIST(root="./downloads", download=True)
         data = d_mnist.data.numpy()
         vec_data = np.reshape(data, (data.shape[0], -1))
         vec_data = np.float32(vec_data)
         labels = d_mnist.targets.numpy()
 
+    # translate all the data to the origin
+    vec_data = vec_data - np.mean(vec_data, 0)
+    norm1 = np.linalg.norm(vec_data)
+    if norm1 == 0:
+        raise ValueError("Input matrices must contain >1 unique points")
+    # change scaling of data (in rows) such that trace(mtx*mtx') = 1
+    vec_data /= norm1
+
     if 0 < n_samples < vec_data.shape[0]:
         rand_indices = np.random.choice(vec_data.shape[0], size=(n_samples,), replace=False)
         return vec_data[rand_indices], labels[rand_indices]
     else:
-        return vec_data, labels
+        rand_indices = np.random.choice(vec_data.shape[0], size=(vec_data.shape[0],), replace=False)
+        return vec_data[rand_indices], labels[rand_indices]
 
 
 def select_test_dataset(dataset_name, testing=False):
@@ -212,13 +220,13 @@ def select_test_dataset(dataset_name, testing=False):
     1 is label of the sample.
     """
     if dataset_name == 'fmnist':
-        f_mnist = FashionMNIST(root="./datasets", train=False, download=True)
+        f_mnist = FashionMNIST(root="./downloads", train=False, download=True)
         data = f_mnist.data.numpy()
         vec_data = np.reshape(data, (data.shape[0], -1))
         vec_data = np.float32(vec_data)
         labels = f_mnist.targets.numpy()
     elif dataset_name == 'usps':
-        f_mnist = USPS(root="./datasets", train=False, download=True)
+        f_mnist = USPS(root="./downloads", train=False, download=True)
         data = f_mnist.data
         vec_data = np.reshape(data, (data.shape[0], -1))
         vec_data = np.float32(vec_data)
@@ -235,7 +243,7 @@ def select_test_dataset(dataset_name, testing=False):
         vec_data, labels = data[2], data[3]
     else:
         print('The dataset you asked for is not available. Gave you MNIST instead.')
-        d_mnist = MNIST(root="./datasets", train=False, download=True)
+        d_mnist = MNIST(root="./downloads", train=False, download=True)
         data = d_mnist.data.numpy()
         vec_data = np.reshape(data, (data.shape[0], -1))
         vec_data = np.float32(vec_data)
@@ -249,7 +257,7 @@ def select_test_dataset(dataset_name, testing=False):
 
 if __name__ == '__main__':
 
-    vec_data, labels = select_dataset('gmm', n_samples=1000)
+    vec_data, labels = select_dataset('aggregation', n_samples=-1)
     print(vec_data.shape)
     print(vec_data[0])
 
